4b256b3b7cfaa48fbf0ae24cfb53e2b1b384a86d
saesac <saesacdev@gmail.com>
2022-03-19 22:52:19 +0900

    edit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '리엑트로 블로그 만들기';
+  function 함수() {
+    return 100;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕하세요
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <div>Dev Blog</div>
+      </div>
+      <h4> { posts } </h4>
     </div>
   );
 }
