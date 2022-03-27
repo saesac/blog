109694d2ec46c8752e251014b3ac457145a10e69
saesac <saesacdev@gmail.com>
2022-03-27 10:43:24 +0900

    Edit UI

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import './App.css';
 
 function App() {
 
-  let [title, titleChange] = useState('남자 코트 추천');
-
-  let posts = '강남 고기 맛집';
+  let [title, titleChange] = useState(['남자 코트 추천', '강남 우동 맛집', '부산 국밥 맛집']);
 
   return (
     <div className="App">
+
       <div className='black-nav'>
         <div>Dev Blog</div>
       </div>
+
       <div className='list'>
-        <h3> { title } </h3>
-        <p>2월 17일 발행</p>
+        <h3> { title[0] } </h3>
+        <p>2022.03.25 발행</p>
         <hr/>
       </div>
+
+      <div className='list'>
+        <h3> { title[1] } </h3>
+        <p>2022.03.26 발행</p>
+        <hr/>
+      </div>
+
+      <div className='list'>
+        <h3> { title[2] } </h3>
+        <p>2022.03.27 발행</p>
+        <hr/>
+      </div>
+
     </div>
   );
 }
