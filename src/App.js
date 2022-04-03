87d3cbf4e3c8ca5b8e520fc5710fae218c016aa0
saesac <saesacdev@gmail.com>
2022-04-03 10:41:03 +0900

    Like button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+/*eslint-disable*/
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
   let [title, titleChange] = useState(['남자 코트 추천', '강남 우동 맛집', '부산 국밥 맛집']);
+  let [like, likeChange] = useState(0);
 
   return (
     <div className="App">
@@ -12,9 +13,9 @@ function App() {
       <div className='black-nav'>
         <div>Dev Blog</div>
       </div>
-
+      {/* <button onClick={ ()=>{ titleChange(['여자 코트 추천', title[1], title[2]]) } }>버튼</button> */}
       <div className='list'>
-        <h3> { title[0] } </h3>
+        <h3> { title[0] } <span onClick={ ()=>{ likeChange(like+1) } }>👍</span> { like } </h3>
         <p>2022.03.25 발행</p>
         <hr/>
       </div>
