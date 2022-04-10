fffa1eaadc27f8fbbd295c066df6d77eb2f08c60
saesac <saesacdev@gmail.com>
2022-04-10 10:37:16 +0900

    Edit

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ function App() {
   let [title, titleChange] = useState(['남자 코트 추천', '강남 우동 맛집', '부산 국밥 맛집']);
   let [like, likeChange] = useState(0);
 
+  function titleBtn() {
+    let newArray = [...title]; // deep copy [...stateName]
+    newArray[0] = '여자 코트 추천'; // edit element
+    titleChange(newArray); // update state
+  }
+  
   return (
     <div className="App">
 
       <div className='black-nav'>
         <div>Dev Blog</div>
       </div>
-      {/* <button onClick={ ()=>{ titleChange(['여자 코트 추천', title[1], title[2]]) } }>버튼</button> */}
+      <button onClick={ titleBtn }>버튼</button>
       <div className='list'>
         <h3> { title[0] } <span onClick={ ()=>{ likeChange(like+1) } }>👍</span> { like } </h3>
         <p>2022.03.25 발행</p>
